cb4785318fdf88409c3c8fc8f57ff7b7b16e978c
omarhaassaan <omarhassan321@hotmail.com>
2019-12-25 15:42:28 +0200

    Registration and Dish

--- Food delivery/src/food/delivery/FoodDelivery.java
+++ Food delivery/src/food/delivery/FoodDelivery.java
@@ -15,7 +15,8 @@ public class FoodDelivery {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Registration rg = new Registration();
+        Login lg = new Login();
     }
     
 }
---
+++ Food delivery/src/food/delivery/Registration.java
@@ -0,0 +1,79 @@
+package food.delivery;
+import java.util.Scanner;
+
+public class Registration extends Form {
+
+    private String username;
+    private String password;
+    private String location;
+    private int ID;
+    private static int numberOfObjects = 0;
+    Scanner sc = new Scanner(System.in);
+
+    Registration() {
+        form();
+    }
+
+    public void RegisterForm() {
+        System.out.println(" ***************   Registration Form *****************");
+        System.out.println("Username: ");
+        super.setUsername(sc.next());
+        System.out.println("Password: ");
+        super.setPassword(sc.next());
+        System.out.println("Location: ");
+        this.location = sc.next();
+
+    }
+
+    @Override
+    public void getInfo() {
+        this.RegisterForm();
+    }
+
+    @Override
+    public void commWithDb() {
+
+        System.out.println("DB: ");
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public static void setNumberOfObjects(int numberOfObjects) {
+        Registration.numberOfObjects = numberOfObjects;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+}
