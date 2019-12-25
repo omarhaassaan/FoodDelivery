5bf7ebc36ec9ec9199bf647514d1dd5f788a13eb
Islam Ashraf <islamashraf710@yahoo.com>
2019-12-25 15:14:59 +0200

    Login

---
+++ Food delivery/src/food/delivery/Form.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food.delivery;
+
+/**
+ *
+ * @author Lenovo
+ */
+public abstract class Form {
+
+    private String username, password;
+
+    Form() {
+        getInfo();
+        commWithDb();
+    }
+
+    public abstract void getInfo();
+
+    public abstract void commWithDb();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ Food delivery/src/food/delivery/Login.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food.delivery;
+
+import food.delivery.Form;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Login extends Form {
+
+    private Scanner cin = new Scanner(System.in);
+
+    Login() {
+        form();
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Enter username");
+        setUsername(cin.next());
+        System.out.println("Enter password");
+        setPassword(cin.next());
+    }
+
+    @Override
+    public void commWithDb() {
+        
+    }
+}
