0b069abd1db96a42633619b2035ca1b1db2014b3
Islam Ashraf <islamashraf710@yahoo.com>
2019-12-25 15:33:17 +0200

    Database

---
+++ Food delivery/src/food/delivery/Database.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food.delivery;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+public class Database {
+    private  static Database instance = null;
+    private Database (){
+        
+    }
+    public static Database getInstance(){
+        if(instance == null)
+            instance = new Database();
+        return instance;
+    }
+}
